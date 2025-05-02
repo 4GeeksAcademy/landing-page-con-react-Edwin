d40be8f1ae7f8c80f85cbb1a9f272f5b15a2845d
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-05-02 17:50:31 +0000

    creados componentes Card y Navbar

---
+++ src/js/components/Card.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import '../../styles/card.css';
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+
+
+//create your first component
+const Card = () => {
+  return (
+    <div className="card custom-card">
+        <img src={rigoImage} />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
