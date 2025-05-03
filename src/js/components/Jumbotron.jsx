e8a5b57889fc556cbf637b8cd7473ce46d82b915
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-05-03 23:38:28 +0000

    agregado texto descriptivo a jumbotron

--- src/js/components/Jumbotron.jsx
+++ src/js/components/Jumbotron.jsx
@@ -4,12 +4,15 @@ import '../../styles/jumbotron.css';
 //create your first component
 const Jumbotron = () => {
   return (
-    <div className="card jumbotron">
+    <div className="card jumbotron bg-light pb-4">
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
+        <h1 className="card-title">Jumbotron</h1>
         <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Este Jumbotron se creó utilizando una tarjeta de Bootstrap con la clase 
+          <code> card</code>. Además, se personalizó con la clase <code>jumbotron </code> 
+          definida en el archivo CSS para que ocupe el 100% del ancho disponible. 
+          También se añadieron clases como <code>bg-light</code> para el fondo claro 
+          y <code>pb-4</code> para agregar un margen inferior.
         </p>
         <a href="#" className="btn btn-primary">
           Go somewhere
